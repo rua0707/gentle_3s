a78b581b6004a48a813a377742d392ff4fbd0882
JAEWOOSUN <wodn1892@naver.com>
2022-05-10 21:13:33 +0900

    Revert "Merge pull request #4 from rua0707/main"
    This reverts commit 02357978c79ec7909e6b0870af2dda5f87fae61b, reversing
    changes made to 113b98166749b83026792d9c2ea1b32bcc743b5c.

--- baekjoon_2163/baekjoon_2163_seungkyoum.py
+++
@@ -1,6 +0,0 @@
-N, M = map(int, input().split())
-print(0 if N*M == 1 else (N*M)-1)
-if N*M == 1:
-    print(0)
-else:
-    print((N*M)-1)
--- baekjoon_2525/baekjoon_2525_seungkyoum.py
+++
@@ -1,8 +0,0 @@
-H, M = map(int, input().split())
-r_time = int(input())
-
-if (M + r_time) >= 60:
-    H += ((M + r_time) // 60)
-    M = (M + r_time) - ((M + r_time) // 60 * 60)
-else:
-    M += r_time
