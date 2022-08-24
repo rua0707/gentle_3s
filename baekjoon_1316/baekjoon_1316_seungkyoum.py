3c5eb635e7971c97318c72314c8496ea30f5ecb9
SeungKim <46487213+rua0707@users.noreply.github.com>
2022-08-24 22:15:50 +0900

    8월4째주_1(수정)

--- baekjoon_1316/baekjoon_1316_seungkyoum.py
+++ baekjoon_1316/baekjoon_1316_seungkyoum.py
@@ -7,9 +7,9 @@ res = N
 for _ in range(N):
     word = sys.stdin.readline().strip()
     for i in range(len(word) - 1):
-        if word[i] == word[i+1]:
+        if word[i] == word[i+1]: # 먼저 같은지 체크
             pass
-        elif word[i] in word[i+1:]:
+        elif word[i] in word[i+1:]: # 다음 문자와 같지 않은 상태에서, 이후에 문자에 같은게 있는지 체크
             res -= 1
             break
 
